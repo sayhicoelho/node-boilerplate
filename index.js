9d0cab8a507439c698989f9c485ce16053816b8a
Renan Coelho <sayhicoelho@gmail.com>
2019-08-12 23:14:30 -0300

    Move files to src/ folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const router = require('./routes')
+const router = require('./src/routes')
 const app = express()
 
 app.use(cors())
