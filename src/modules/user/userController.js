fea987ea9363c1b06ce9c26e81278b66c7937c88
Renan Coelho <sayhicoelho@gmail.com>
2019-08-14 01:07:27 -0300

    Add events

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -1,12 +1,18 @@
 const userService = require('./userService')
+const userEvents = require('./userEvents')
 
 const index = (req, res) => res.json({ users: userService.getUsers() })
 
 const getUser = (req, res) =>
   res.json({ user: userService.getUser(req.params.id) })
 
-const createUser = (req, res) =>
-  res.json({ user: userService.createUser(req.body) })
+const createUser = (req, res) => {
+  const user = userService.createUser(req.body)
+
+  res.json({ user })
+
+  userEvents.emit('user.registered', user)
+}
 
 module.exports = {
   index,
